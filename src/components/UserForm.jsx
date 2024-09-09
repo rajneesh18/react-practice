cc8f0a55df6b0b9cb50f7b97edddc39109fe4ff7
Rajneesh Shukla <shukla.rajneesh2018@gmail.com>
2024-09-09 18:48:55 +0530

    Showing Specific Validation Errors using useActionState

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useActionState  } from 'react'
 import { Form, Button } from 'react-bootstrap'
 
 // Added bootstrap CSS
@@ -10,16 +10,47 @@ const UserForm = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
+    const handleSubmit = (state, formData) => {
+        setEmail(`${formData.get("email")}`)
+        setPassword(`${formData.get("password")}`)
+
+        // Vailidate Email
+        let emailValid = false;
+        if(formData.get("email").length === 0) state.emailError = "Email is required"
+        else if(formData.get("email").length < 6) {
+            state.emailError = "Email should be minimum 6 Characters"
+        } 
+        else if(formData.get("email").indexOf('') > 0) {
+            state.emailError = "Email Cannot contain spaces"
+        } else {
+            state.emailError = ""
+            emailValid = true
+        }
+
+        if(emailValid) {
+            alert(`Email: ${email} \n Password: ${password}`)
+        }
+        console.log(state);
+        return state;
+    }
+
+    const [state, formAction] = useActionState(handleSubmit, {
+        emailError: ""
+    })
+    console.log(state);
+
+
     return (
         <div>
-            <Form>
+            <Form action={formAction} >
                 <Form.Group className='mb-3' controlId="formBasicEmail" >
                     <Form.Label>Email Address</Form.Label>
                         <Form.Control 
                             type="email" 
                             placeholder='Enter email' 
                             name="email" 
-                            onChange={event => setEmail(event.target.value)} />
+                            onChange={event => setEmail(event.target.value)}
+                             />
 
                     <Form.Text className='text-muted'>
                         We'll never share your email with anyone else.
@@ -32,7 +63,8 @@ const UserForm = () => {
                             type='password' 
                             placeholder='Password' 
                             name="password"
-                            onChange={event => setPassword(event.target.value)} />
+                            onChange={event => setPassword(event.target.value)} 
+                            />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">Submit</Button>
@@ -46,3 +78,5 @@ const UserForm = () => {
 }
 
 export default UserForm
+
+/** Showing Specific Validation Errors */
